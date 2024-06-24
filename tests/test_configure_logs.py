9a58ee2fc61f17f48e8706e67f905aa59e2ca2f3
Darian <daj-samsoedien@live.nl>
2024-06-24 21:01:26 +0200

    ADded asserts 2,0

--- tests/test_configure_logs.py
+++ tests/test_configure_logs.py
@@ -1,5 +1,6 @@
-from nikola.log import configure_logging, print_coverage_configure, reset_coverage_configure, LoggingMode
+from nikola.log import configure_logging, print_coverage_configure, reset_coverage_configure, LoggingMode,  StrictModeExceptionHandler
 from unittest.mock import patch
+import logging
 import pytest
 
 @pytest.mark.skip(reason="None")
@@ -7,12 +8,24 @@ def test_normal_mode_debug():
     #reset_coverage_configure()
     with patch('nikola.log.DEBUG', True):
         configure_logging(logging_mode=LoggingMode.NORMAL)
+        assert logging.root.level == logging.DEBUG
+        foundHandler = False
+        for handler in logging.root.handlers:
+            if isinstance(handler, logging.StreamHandler):
+                foundHandler = True
+        assert foundHandler
         #print_coverage_configure()
         
 @pytest.mark.skip(reason="None")
 def test_normal_mode_no_debug():
     #reset_coverage_configure()
     configure_logging(logging_mode=LoggingMode.NORMAL)
+    assert logging.root.level == logging.INFO
+    foundHandler = False
+    for handler in logging.root.handlers:
+        if isinstance(handler, logging.StreamHandler):
+            foundHandler = True
+    assert foundHandler
     #print_coverage_configure()
 
 @pytest.mark.skip(reason="None")
@@ -20,12 +33,16 @@ def test_quiet_mode_debug():
     #reset_coverage_configure()
     with patch('nikola.log.DEBUG', True):
         configure_logging(logging_mode=LoggingMode.QUIET)
+        assert logging.root.level == logging.DEBUG
+        assert not logging.root.handlers
         #print_coverage_configure()
   
 @pytest.mark.skip(reason="None")
 def test_quiet_mode_no_debug():
     #reset_coverage_configure()
     configure_logging(logging_mode=LoggingMode.QUIET)
+    assert logging.root.level == logging.INFO
+    assert not logging.root.handlers
     #print_coverage_configure()
 
 @pytest.mark.skip(reason="None")
@@ -33,12 +50,32 @@ def test_strict_mode_debug():
     #reset_coverage_configure()
     with patch('nikola.log.DEBUG', True):
         configure_logging(logging_mode=LoggingMode.STRICT)
+        assert logging.root.level == logging.DEBUG
+        foundHandler = False
+        foundHandler2 = False 
+        for handler in logging.root.handlers:
+            if isinstance(handler, logging.StreamHandler):
+                foundHandler = True
+            if isinstance(handler, StrictModeExceptionHandler):
+                foundHandler2 = True
+        assert foundHandler
+        assert foundHandler2
         #print_coverage_configure()
 
 @pytest.mark.skip(reason="None")
 def test_strict_mode_no_debug():
     #reset_coverage_configure()
     configure_logging(logging_mode=LoggingMode.STRICT)
+    assert logging.root.level == logging.INFO
+    foundHandler = False
+    foundHandler2 = False 
+    for handler in logging.root.handlers:
+        if isinstance(handler, logging.StreamHandler):
+            foundHandler = True
+        if isinstance(handler, StrictModeExceptionHandler):
+            foundHandler2 = True
+    assert foundHandler
+    assert foundHandler2
     #print_coverage_configure()
     
     
