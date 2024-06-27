76cfa5cd5b54faa7061dd04e323a83cdc3406a6a
Abdul Aziz Hatkou <zizochampion@hotmail.com>
2024-06-27 17:24:17 +0200

    added asserts to test_custom.py

--- tests/test_custom.py
+++ tests/test_custom.py
@@ -13,10 +13,18 @@ def test_color_formatter():
     formatter = ColorfulFormatter()
     # Test cases for each logging level and the colorful toggle
     test_cases = [
+        (logging.DEBUG, False), (logging.DEBUG, True),
         (logging.INFO, False), (logging.INFO, True),
         (logging.WARNING, False), (logging.WARNING, True),
         (logging.ERROR, False), (logging.ERROR, True)
     ]
+
+    dict_colors = {
+        logging.DEBUG: "\033[37m{}\033[0m",
+        logging.ERROR: "\033[1;31m{}\033[0m",
+        logging.WARNING: "\033[1;33m{}\033[0m",
+        logging.INFO: "\033[1m{}\033[0m",
+    }
 
     for level, colorState in test_cases:
         # Create a LogRecord for the current level
@@ -32,9 +40,14 @@ def test_color_formatter():
 
         formatter._colorful = colorState
 
-        # Call wrap_in_color and print the result for verification
+        # Call format and print the result for verification
         formatted_message = formatter.format(log_record)
         print(f"Level: {log_record.levelname}, Colorful: {colorState}, Output: {formatted_message}")
+        
+        if colorState:
+            assert formatted_message == dict_colors[level].format("Test message")
+        else:
+            assert formatted_message == "Test message"
     
     ColorfulFormatter.print_colorful_wrap_coverage()
     
